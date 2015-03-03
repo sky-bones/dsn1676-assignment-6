c7a1609b11dcfcce4cf300ced96aa199355942a8
Elizabeth Nichols <elizabeth.nichols.95@gmail.com>
2015-03-02 19:39:10 -0500

    Added in Shapes
    first 4 shapes have been added,
    added animations and transitions

---
+++ js/main.js
@@ -0,0 +1,42 @@
+var $btnShwHide = $('.btn-show-hide');
+var $box = $('.box');
+
+$btnShwHide.on('click', function () {
+
+$box.toggleClass('js-btn-show-hide');
+
+});
+
+
+
+var $btnMove = $('.btn-move');
+var $diamond = $('.diamond');
+
+$btnMove.on('click', function (){
+	$diamond.toggleClass('js-btn-move');
+});
+
+
+var $btnCollapseExpand = $('.btn-collapse-expand');
+var $panel = $('.panel');
+
+$btnCollapseExpand.on('click', function (){
+	$panel.toggleClass('js-panel');
+});
+
+var $btnBounce = $('.btn-bounce');
+var $circle = $('.circle')
+
+$btnBounce.on('click', function () {
+	$circle.addClass('js-circle-bounce');
+});
+
+$circle.on('animationend', function () {
+	$circle.removeClass('js-circle-bounce');
+});
+
+
+var $btnAppend = $('.btn-append');
+
+
+
