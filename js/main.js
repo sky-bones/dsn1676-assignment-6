18ee6d3f3f02f3dc6b795513e1dd33b3767815f0
Elizabeth Nichols <elizabeth.nichols.95@gmail.com>
2015-03-09 16:43:29 -0400

    Added in final transitions
    cleaned up code

--- js/main.js
+++ js/main.js
@@ -8,11 +8,10 @@ $box.toggleClass('js-btn-show-hide');
 });
 
 
-
 var $btnMove = $('.btn-move');
 var $diamond = $('.diamond');
 
-$btnMove.on('click', function (){
+	$btnMove.on('click', function (){
 	$diamond.toggleClass('js-btn-move');
 });
 
@@ -20,29 +19,33 @@ $btnMove.on('click', function (){
 var $btnCollapseExpand = $('.btn-collapse-expand');
 var $panel = $('.panel');
 
-$btnCollapseExpand.on('click', function (){
+	$btnCollapseExpand.on('click', function (){
 	$panel.toggleClass('js-panel');
 });
 
 var $btnBounce = $('.btn-bounce');
-var $circle = $('.circle')
+var $circle = $('.circle');
 
-$btnBounce.on('click', function () {
+	$btnBounce.on('click', function () {
 	$circle.addClass('js-circle-bounce');
 });
 
-$circle.on('animationend', function () {
+	$circle.on('animationend', function () {
 	$circle.removeClass('js-circle-bounce');
 });
-
 
 var $btnAppend = $('.btn-append');
 var $list = $('.list');
 
-$btnAppend.on('click', function () {
-	$list.addClass('js-list');
+
+	$btnAppend.on('click', function () {
+	var $li = $('<li>').html('New');
+	$li.addClass('btn-append');
+	$list.append($li);
 
 });
 
-
-
+	// create a new li
+	// put some content in that li
+	// add the li to the list
+	// add class to li
